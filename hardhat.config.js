1e6cf049bafd77b4bfb07b74a8a2ccd107e0cc87
antoniobp <antonio@bitpagos.co>
2023-03-27 15:51:32 -0300

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("./tasks/register-entity");
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      gasPrice: 30000000000,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
